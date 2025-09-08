0b7377e53c552e240fdeefff704b7dc19122437d
Sonelilyn22 <kalerworld23@gmail.com>
2025-09-08 20:03:05 +0400

    add statisticmethods

---
+++ dataframe.py
@@ -0,0 +1,76 @@
+import pandas as pd
+import datetime
+
+
+#Проверка существования dataframe
+def check_statistic():
+    try:
+        df = pd.read_csv('dataframes/statistic.csv')
+    except:
+        df = pd.DataFrame(columns=['chat_id','user_id','fullname','message','type','date'])
+        df.to_csv('dataframes/statistic.csv',index=False)
+
+#Получение dataframe
+def get_statistic() -> pd.DataFrame:
+    return pd.read_csv('dataframes/statistic.csv')
+
+#Добавление новых записей в dataframe
+def save_statistic(chat_id,user_id,fullname,message,type):
+    date = datetime.date.today()#Генерация текущей даты
+    new_message = {
+        'chat_id':chat_id,
+        'user_id':user_id,
+        'fullname':fullname,
+        'message':message,
+        'type':type,
+        'date':date
+    }#Новая запись
+    df = get_statistic()
+    df.loc[len(df)] = new_message #Добавление новой записи в dataframe
+    df.to_csv('dataframes/statistic.csv',index=False)
+
+
+###### Функции статистики #######
+
+
+#Получение кол-ва сообщений у конкретного пользователя
+def get_user_messages_count(chat_id,user_id) -> str:
+    df = get_statistic()
+    df = df[(df['chat_id'] == chat_id) & (df['user_id'] == user_id)]
+    fullname = df['fullname'][0]
+    return f'{fullname} - {len(df)}'
+
+#Получение средней длины сообщений конкретного пользователя
+def get_user_messages_len(chat_id,user_id) -> str:
+    df = get_statistic()
+    df = df[(df['chat_id'] == chat_id) & (df['user_id'] == user_id)]
+    fullname = df['fullname'][0]
+    messages_len = sum(len(x) for x in df['message'].values)
+    messages_count = len(df)
+    average = messages_len / messages_count
+    return f'{fullname} - {round(average,2)}'
+
+def get_group_messages_count(chat_id) -> str:
+    df = get_statistic()
+    df = df[(df['chat_id'] == chat_id)]
+    df = df.groupby(df['fullname'])
+    result = 'Топ пользователей по кол-ву сообщений \n'
+    for fullname,value in df:
+        result += f'{fullname} - {len(value)} \n'
+    return result 
+
+def get_group_messages_len(chat_id):
+    df = get_statistic()
+    df = df[(df['chat_id'] == chat_id)]
+    df = df.groupby(df['fullname'])
+    result = 'Топ пользователей по средней длине сообщений \n'
+    for fullname,value in df:
+        messages_len = sum(len(x) for x in value['message'].values)
+        messages_count = len(value)
+        average = messages_len / messages_count
+        result += f'{fullname} - {round(average,2)}\n'
+    return result
+
+#Авто запуск при запуске бота
+check_statistic()
+
--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import os
 import dotenv
 import uuid
 import random
+import dataframe
 
 def create_chat_folder(chatId):
     os.makedirs(f'uploads/{chatId}',exist_ok=True)
@@ -54,6 +55,29 @@ def reply_location(message:types.Message):
     keyboard.add(button)
     bot.send_message(message.chat.id,'Дай локацию',reply_markup=keyboard)
 
+@bot.message_handler(commands=['messages_count'])
+def get_user_messages_count(message:types.Message):
+    chat_id = message.chat.id
+    user_id = message.from_user.id
+    result = dataframe.get_user_messages_count(chat_id,user_id)
+    bot.send_message(message.chat.id,result)
+
+@bot.message_handler(commands=['user_stats'])
+def create_user_stat(message:types.Message):
+    keyboard = util.quick_markup({
+        'Кол-во сообщений':{'callback_data':'user_messages_count'},
+        'Средняя длина сообщений':{'callback_data':'user_messages_len'}
+    })
+    bot.send_message(message.chat.id,'Кнопки вашей статстики',reply_markup=keyboard)
+
+@bot.message_handler(commands=['group_stats'])
+def create_group_stat(message:types.Message):
+    keyboard = util.quick_markup({
+        'Кол-во сообщений':{'callback_data':'group_messages_count'},
+        'Средняя длина сообщений':{'callback_data':'group_messages_len'}
+    })
+    bot.send_message(message.chat.id,'Статистика группы',reply_markup=keyboard)
+
 @bot.message_handler(content_types=['photo'])
 def OnSendImage(message:types.Message):
     create_chat_folder(str(message.chat.id))
@@ -79,7 +103,6 @@ def OnSendAudio(message:types.Message):
 
 @bot.message_handler(content_types=['video'])
 def OnSendVideo(message:types.Message):
-
     chatId = str(message.chat.id)
     create_chat_folder(chatId)
     video = message.video
@@ -89,15 +112,36 @@ def OnSendVideo(message:types.Message):
     with open(f'uploads/{chatId}/{filename}.mp4','wb') as newfile:
         newfile.write(file)
     bot.reply_to(message,'Видео сохранено')
-
 @bot.callback_query_handler(func=lambda call:True)
 def callback_handler(call:types.CallbackQuery):
     if call.message:
         if call.data == 'test':
             bot.send_message(call.message.chat.id,'Вы нажали на inline кнопку')
             bot.edit_message_reply_markup(call.message.chat.id,call.message.id,reply_markup=None)
+        if call.data == 'user_messages_count':
+            chat_id = call.message.chat.id
+            user_id = call.from_user.id
+            result = dataframe.get_user_messages_count(chat_id,user_id)
+            bot.send_message(chat_id,result)
+        if call.data == 'user_messages_len':
+            chat_id = call.message.chat.id
+            user_id = call.from_user.id
+            result = dataframe.get_user_messages_len(chat_id,user_id)
+            bot.send_message(chat_id,result)
+        if call.data == 'group_messages_count':
+            chat_id = call.message.chat.id
+            result = dataframe.get_group_messages_count(chat_id)
+            bot.send_message(chat_id,result)
+        if call.data == 'group_messages_len':
+            chat_id = call.message.chat.id
+            result = dataframe.get_group_messages_len(chat_id)
+            bot.send_message(chat_id,result)
+
 @bot.message_handler(func=lambda message:True)
 def all_messages(message:types.Message):
-    bot.reply_to(message,message.text)
+    chat_id = message.chat.id
+    user_id = message.from_user.id
+    fullname = message.from_user.full_name
+    dataframe.save_statistic(chat_id,user_id,fullname,message.text,message.content_type)
 
 bot.infinity_polling()
